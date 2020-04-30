41010a148c863dc25f4f4ba48a5413eac2e7cae0
myname <jdgray@protonmail.com>
2020-04-30 19:08:41 -0400

    Use correct folder name for "pack". Don't initialize repo folders before cloning.

--- create_dot_vim.sh
+++ create_dot_vim.sh
@@ -7,7 +7,7 @@ if [ ! -d $dot_vim_dir ]; then
 fi
 cd $dot_vim_dir || exit 1;
 
-declare -a dir_tree=("testdir" "my-plugins" "start")
+declare -a dir_tree=("pack" "my-plugins" "start")
 for i in "${dir_tree[@]}"
 do
   mkdir "$i" && cd "$i" || exit 1;
@@ -18,17 +18,10 @@ if [ "$(basename "$(pwd)")" != "start" ]; then
   exit 1;
 fi
 
-declare -a repos=("gruvbox" "dracula" "ultisnips" "vim-commentary" "vim-snippets")
-for i in "${repos[@]}"
-do
-  mkdir "$i" || exit 1;
-done
-
-cd "gruvbox" && git clone "https://github.com/morhetz/gruvbox.git";
-
-cd "../dracula" && git clone "https://github.com/dracula/vim.git";
-cd "../vim-commentary" && git clone "https://github.com/tpope/vim-commentary.git";
-cd "../ultisnips" && git clone "https://github.com/SirVer/ultisnips.git";
-cd "../vim-snippets" && git clone "https://github.com/honza/vim-snippets.git";
+git clone "https://github.com/morhetz/gruvbox.git";
+git clone "https://github.com/dracula/vim.git";
+git clone "https://github.com/tpope/vim-commentary.git";
+git clone "https://github.com/SirVer/ultisnips.git";
+git clone "https://github.com/honza/vim-snippets.git";
 
 echo "done";
